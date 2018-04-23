c514ccc5c36d7f6fcc02ca73872c71dc33f2a8d5
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-04-23 08:23:25 -0400

    Updated from Brython Server: 4/23/2018 8:23:24 AM

--- fwordDemo.py
+++ fwordDemo.py
@@ -1,1 +1,9 @@
+#Daniel Bandler
+#4/23/18
+#Asks user for a bunch of words, only prints ones with the letter "f"
 
+words = input("Type in words ").split(" ")
+
+for item in words:
+    if "f" in item or "F" in item:
+        print(item)
