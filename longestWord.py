846c72b3503427f6e4c63a85776de8275b8ed8e8
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-04-23 08:51:33 -0400

    Updated from Brython Server: 4/23/2018 8:51:33 AM

--- longestWord.py
+++ longestWord.py
@@ -1,1 +1,11 @@
+#Daniel Bandler
+#4/23/18
+#longestWord.py picks longest word in string
 
+words = input("Input words here ").split(" ")
+
+longest = ""
+for i in words:
+    if len(i) > longest:
+        longest = words
+        
