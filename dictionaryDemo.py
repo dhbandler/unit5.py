0dc591b7fe144ff790983864e35ac6f69c0a823a
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-04-25 08:11:23 -0400

    Updated from Brython Server: 4/25/2018 8:11:22 AM

--- dictionaryDemo.py
+++ dictionaryDemo.py
@@ -1,1 +1,14 @@
+#Daniel Bandler
+#4/25/18
+#dictionaryDemo.py --- more list practice
 
+words = ["computer", "mortify", "dog", "firetruck", "yes", "python", "cat"]
+
+words.sort()
+
+num = int(input("what number word do ya want? "))
+
+if num <= 0 or num>=len(words)+1:
+    print("Invalid number you degenerate swine")
+else:
+    print(words[num-1])
