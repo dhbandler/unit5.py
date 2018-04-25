fa3fe8ed4782181672d2e66342f8f7198ce68331
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-04-25 08:20:09 -0400

    Updated from Brython Server: 4/25/2018 8:20:08 AM

--- warmup13.py
+++ warmup13.py
@@ -1,1 +1,13 @@
+#Daniel Bandler
+#4/25/18
+#warmup13.py makes list of random numbers and does things with them
 
+from random import randint
+L = [0]
+for i in range(1,21):
+    L.append(randint(1,101))
+L.sort()
+
+print(sum(L))
+print(min(L))
+print(max(L))
