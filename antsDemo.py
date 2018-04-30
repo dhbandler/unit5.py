8bbcf19e1cea14a96da67fd7af36e61376ed7002
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-04-30 08:18:50 -0400

    Updated from Brython Server: 4/30/2018 8:18:50 AM

--- antsDemo.py
+++ antsDemo.py
@@ -1,1 +1,31 @@
+#Daniel Bandler
+#4/30/18
+#antsDemo.py --- using lists with graphics
 
+from ggame import *
+from random import randint
+
+ANTS = 100
+WIDTH = 800
+HEIGHT = 450
+
+#move each ant randomly up/down and left right
+def step():
+    for ant in data["antlist"]:
+        ant.x += randint(-4,3)
+        ant.y += randint(-4,3)
+        
+
+#putting FIRE ants randomly on the screen
+if __name__ == "__main__":
+    
+    data = {}
+    data["antlist"] = []
+    red = Color(0xFF0000,1)
+    black = Color(0x000000,1)
+    ant = CircleAsset(20,LineStyle(1,black), red)
+    
+    for i in range(ANTS):
+        data["antlist"].append(Sprite(ant,(randint(1,WIDTH),randint(1,HEIGHT))))
+    
+    App().run(step)
